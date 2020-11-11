3c43ab0d0aab08a47e7a9a238012589b23559575
Turkyden <wj871287@gmail.com>
2020-11-11 20:43:24 +0800

    chore: add the index.d.ts in dist
    affects: react-typing-hook, react-typing-storybook

--- packages/react-typing-hook/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { RefObject } from "react";
-import './index.css';
-/**
- * React Hook Wrapper
- * @param ref
- * @param param1
- */
-export default function useTyping(ref: RefObject<HTMLDocument | null>, { steps, loop, speed }: TypingOptions): void;
--- packages/react-typing-hook/src/index.ts
+++ packages/react-typing-hook/src/index.ts
@@ -1,3 +1,5 @@
+///<reference path="index.d.ts"/>
+
 import { useEffect, RefObject } from "react";
 import './index.css'
 
